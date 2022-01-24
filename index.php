58d4a8bc0d558030d9430cadaa95342af8673f02
MaksaDev <jokic.maksim17@gmail.com>
2022-01-24 15:02:57 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html> 
+
+<html lang="en">
+
+<head>
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boogle Search</title>
+</head>
+
+<body>
+    
+<div class="wrapper indexPage">
+
+
+    <div class="mainSection">
+
+        <div class="logoContainer">
+
+            <img src="assets//images/boogle.png" >
+        </div>
+
+        <div class="searchContainer">
+
+            <form action="search.php" method="GET">
+
+                <input class="searchBox" type="text" name="term">
+                <input class="searchButton" type="submit" value="Search">
+
+            </form>   
+        </div>
+
+    
+    </div>
+
+
+</div>
+
+</body>
+
+
+</html>
---
+++ search.php
@@ -0,0 +1,52 @@
+
+<!DOCTYPE html> 
+
+<html lang="en">
+
+<head>
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boogle Search</title>
+</head>
+
+<body>
+    
+<div class="wrapper ">
+
+    <div class="header">
+        <div class="headerContent">
+            <div class="logoContainer">
+
+                 <a href="index.php"> <img src="assets//images/boogle.png" > </a>
+            </div>
+
+            <div class="searchContainer">
+                <form action="search.php" method="GET">
+                    <div class="searchBarContainer">
+                        
+                        <input class="searchBox" type="text" name="term">
+
+                        <button class="searchButton">
+                            <img src="assets/images/icons/slikaBTN.png" >
+                        </button>
+                    </div>
+
+                </form>
+
+            </div>
+
+        </div>
+    </div>
+
+
+</div>
+
+</body>
+
+
+</html>
+
