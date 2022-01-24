3d4fcf66a4aaa9b8b40de89524e627243950b56f
MaksaDev <jokic.maksim17@gmail.com>
2022-01-24 16:43:05 +0100

    img+site tabs

--- search.php
+++ search.php
@@ -1,3 +1,16 @@
+<?php
+
+    if(isset($_GET["term"])){
+        $term = $_GET["term"];
+    }
+    else{
+        exit("Moras uneti search term");
+    }
+
+    $type = isset($_GET["type"]) ? $_GET["type"] : 'sites' ;
+    
+
+?>
 
 <!DOCTYPE html> 
 
@@ -40,6 +53,30 @@
             </div>
 
         </div>
+
+        <div class="tabsContainer">
+            <ul class="tabList">
+
+                <li class=" <?php  echo $type == 'sites' ? 'active' : '' ;?> " > 
+                    <a   href='   <?php echo "search.php?term=$term&type=sites"; ?>   '   >
+                        Sites
+                    </a>
+                </li>
+
+                <li class="<?php echo $type == 'images' ? 'active' : '' ;?> "> 
+                    <a   href='   <?php echo "search.php?term=$term&type=images" ;?>  '   >
+                        Images
+                    </a>
+                </li>
+                
+
+            </ul>
+        </div>
+
+
+
+
+
     </div>
 
 
