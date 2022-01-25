c44287220de59ff640579d274566f9d7de64d1f7
MaksaDev <jokic.maksim17@gmail.com>
2022-01-25 18:32:42 +0100

    link za linkom +

--- crawl.php
+++ crawl.php
@@ -2,8 +2,36 @@
 
 include("classes/DomDocumentParser.php");
 
+$alreadyCrawled = array();
+$crawling = array();
+
+
+
+    function createLink($src, $url){
+
+            $scheme = parse_url($url)["scheme"];  //  http
+            $host = parse_url($url)["host"];  //  www....com
+
+
+        if(substr($src , 0, 2) == "//"){
+            $src = $scheme . ":" . $src;
+        }
+            else if (substr($src , 0, 1) == "/"){
+                $src = $scheme . "://" . $host . $src;
+
+            }
+         
+        return $src;
+    }
+
+
+
 
     function followLinks($url){
+
+        global $alreadyCrawled;
+        global $crawling;
+
 
         $parser = new DomDocumentParser($url);
         
@@ -11,13 +39,40 @@ include("classes/DomDocumentParser.php");
 
         foreach($linkList as $link){
             $href = $link->getAttribute("href");
+
+            if((strpos($href , "#") !== false) || (substr($href , 0, 1) == "?")) {
+                continue;
+            }
+            else if(substr($href , 0, 11) == "javascript:"){
+                continue;
+            }
+            
+            $href  = createLink($href , $url );
+
+        
+
+            if( !in_array($href, $alreadyCrawled ) ){
+                $alreadyCrawled[] = $href;
+                $crawling[] = $href;
+
+
+                //insert $href to db
+            }
+
             echo $href . "<br>";
+        }
+
+
+        array_shift($crawling);
+        foreach($crawling as $site){
+            followLinks($site);
+        
         }
 
 
     }
 
-$startUrl = "http://www.fon.bg.ac.rs";
+$startUrl = "https://www.python.org/";
 followLinks($startUrl);
 
 
