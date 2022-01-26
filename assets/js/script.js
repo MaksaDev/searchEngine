42a73f802eb42b1410eb9d051a22f9a51ce31d11
MaksaDev <jokic.maksim17@gmail.com>
2022-01-26 18:34:53 +0100

    fix plus slanje na stranicu

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,16 @@ $(".result").on("click", function() {
 
 function increaseLinkClicks(linkId, url){
 
-    $.post("ajax/updateLinkCount.php", {linkId:  linkId});
+    $.post("ajax/updateLinkCount.php", {linkId:  linkId})
+    .done(function(result){
+
+        if(result != ""){
+            alert(result);
+            return;
+        }
+
+        window.location.href = url ;
+    });
 
 
 }
