40075aa313764d46b7922c24853ad411a2beb94b
MaksaDev <jokic.maksim17@gmail.com>
2022-01-26 18:26:40 +0100

    BUG

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    
+$(".result").on("click", function() {
+    console.log("I was clicked");
+
+    var id = $(this).attr("data-linkId");
+    var url = $(this).attr("href");
+
+    console.log(id);
+    if(!id){
+        alert("data-linkId not found WTF"); 
+    }
+
+    increaseLinkClicks(id,url);
+
+
+    return false;
+})
+
+
+});
+
+
+
+function increaseLinkClicks(linkId, url){
+
+    $.post("ajax/updateLinkCount.php", {linkId:  linkId});
+
+
+}
--- search.php
+++ search.php
@@ -23,6 +23,8 @@ include("classes/SiteResultsProvider.php");
 <head>
 
     <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
 
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -161,6 +163,7 @@ include("classes/SiteResultsProvider.php");
 
 </div>
 
+<script type="text/javascript" src="assets/js/script.js"></script>
 </body>
 
 
